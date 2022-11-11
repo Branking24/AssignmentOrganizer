b92373b86bf08a650885165ae07e464976664133
Ubuntu <ubuntu@ip-172-31-26-186.us-west-1.compute.internal>
2022-11-11 01:01:27 +0000

    Touch-ups

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -9,7 +9,7 @@ import AssignmentViewer from './components/assignment-viewer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AssignmentViewer />
+    <App />
   </React.StrictMode>
 );
 
